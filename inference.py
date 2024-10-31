391a20e65b4577840b378e243f828cd0a45859de
yuchen3890 <alice95058@gmail.com>
2024-10-31 11:36:53 +0800

    add profiler

--- inference.py
+++ inference.py
@@ -1,5 +1,6 @@
 import transformers
 from resources.modeling_qwen2 import Qwen2ForCausalLM
+# from torch.profiler import profile, record_function, ProfilerActivity
 
 MODEL_NAME = "Qwen/Qwen2.5-0.5B-Instruct"
 DEVICE = "cpu"
@@ -18,13 +19,16 @@ if __name__ == "__main__":
         return_tensors="pt", add_generation_prompt=True
     ).to(DEVICE)
 
-
+    # with profile(activities=[ProfilerActivity.CPU],
+    #     profile_memory=True, record_shapes=True) as prof:
     generated_ids = model.generate(
         input_ids=model_inputs,
         do_sample=False,
         temperature=None, top_p=None, top_k=None,
         max_new_tokens=512
     )
+    # print(prof.key_averages().table(sort_by="cpu_memory_usage", row_limit=10))
+
     generated_ids = [
         output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs, generated_ids)
     ]
